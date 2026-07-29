f8fc9ed3698836229dc9291f569a554527d10567
newnesscaesar-coder <newnesscaesar@gmail.com>
2026-07-29 16:14:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+﻿const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".site-nav");
+const faqItems = document.querySelectorAll(".faq-item");
+const testimonialSlides = document.querySelectorAll(".testimonial-slide");
+const prevButton = document.querySelector(".testimonial-nav.prev");
+const nextButton = document.querySelector(".testimonial-nav.next");
+let testimonialIndex = 0;
+
+if (navToggle && nav) {
+  navToggle.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+
+  document.querySelectorAll(".site-nav a").forEach((link) => {
+    link.addEventListener("click", () => nav.classList.remove("open"));
+  });
+}
+
+faqItems.forEach((item) => {
+  const button = item.querySelector("button");
+  button.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
+
+function showTestimonial(index) {
+  testimonialSlides.forEach((slide, idx) => {
+    slide.classList.toggle("active", idx === index);
+  });
+}
+
+if (testimonialSlides.length) {
+  showTestimonial(testimonialIndex);
+  if (prevButton && nextButton) {
+    prevButton.addEventListener("click", () => {
+      testimonialIndex = (testimonialIndex - 1 + testimonialSlides.length) % testimonialSlides.length;
+      showTestimonial(testimonialIndex);
+    });
+
+    nextButton.addEventListener("click", () => {
+      testimonialIndex = (testimonialIndex + 1) % testimonialSlides.length;
+      showTestimonial(testimonialIndex);
+    });
+  }
+
+  setInterval(() => {
+    testimonialIndex = (testimonialIndex + 1) % testimonialSlides.length;
+    showTestimonial(testimonialIndex);
+  }, 6000);
+}
+
